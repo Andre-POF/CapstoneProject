de94f83349b64abff1839cfa9dde6724dc5a1d2d
André Ferreira <andreferreiramusic@gmail.com>
2024-07-08 11:33:38 +0200

    uncomment googleUserR

--- backend/services/routes/googleUser.route.js
+++ backend/services/routes/googleUser.route.js
@@ -25,20 +25,20 @@ googleUserRoute.get(
   }
 );
 
-// googleUserRoute.get(
-//   "/googleLogin",
-//   passport.authenticate("google", { scope: ["profile", "email"] })
-// );
+googleUserRoute.get(
+  "/googleLogin",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
 
-// googleUserRoute.get(
-//   "/callback",
-//   passport.authenticate("google", { session: false }),
-//   (req, res, next) => {
-//     try {
-//       res.send(req.user);
-//       res.redirect(`http://localhost:3000/home?accToken=${req.user.accToken}`);
-//     } catch (error) {
-//       next(error);
-//     }
-//   }
-// );
+googleUserRoute.get(
+  "/callback",
+  passport.authenticate("google", { session: false }),
+  (req, res, next) => {
+    try {
+      res.send(req.user);
+      res.redirect(`http://localhost:3000/home?accToken=${req.user.accToken}`);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
